e59d7e465775cc1d22fb827e79dc0772b5592a7e
francijpg <hellofrancijpg@gmail.com>
2020-10-29 10:03:27 -0300

    Create Alerta.js

---
+++ src/components/layout/Alerta.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Alerta = ({ alerta }) => {
+  if (!alerta) return null;
+  return (
+    <div data-cy="alerta" className={`alerta ${alerta.categoria}`}>
+      {alerta.msg}
+    </div>
+  );
+};
+
+export default Alerta;
