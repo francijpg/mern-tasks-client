c984e13eaaa605b1112147769c9d41971aa8d985
francijpg <hellofrancijpg@gmail.com>
2020-10-30 10:27:13 -0300

    Update proyectoState.js

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -59,7 +59,7 @@ const ProyectoState = (props) => {
   const agregarProyecto = async (proyecto) => {
     try {
       const resultado = await clienteAxios.post("/api/proyectos", proyecto);
-      console.log(resultado);
+      // console.log(resultado);
       // Insertar el proyecto en el state
       dispatch({
         type: AGREGAR_PROYECTO,
